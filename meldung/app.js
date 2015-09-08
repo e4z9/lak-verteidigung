576b7b850e26a6a500bea262e105e2c8377db07c
Eike <e4z9mail@gmail.com>
2015-09-08 14:03:45 +0200

    Meldungen: Burgen hinzufügen

--- meldung/app.js
+++ meldung/app.js
@@ -1,11 +1,40 @@
 "use strict";
 angular.module('lakmeldung', [])
-    .controller('appController', function() {
-        this.spielerName = '';
-        this.burgName = '';
-        this.burgLink = '';
+    .controller('appController', function($scope) {
+        this.spielerName = localStorage['e4z9.lak.spielerName'];
+
+        try {
+			this.burgen = JSON.parse(localStorage['e4z9.lak.burgen']);
+	    } catch(e) {
+			this.burgen = [{name: 'Neue Burg', link: ''}];
+	    }
+        this.burg = this.burgen[0];
         this.datum = new Date();
         this.zeit = new Date();
         this.brueckenLink = '';
         this.angreifer = '';
+
+        var that = this;
+
+        var sortiere = function() {
+            that.burgen.sort(function(a, b) {
+                return a.name.localeCompare(b.name);
+            });
+        }
+
+        sortiere();
+
+        this.neueBurg = function() {
+            that.burgen.push({name: 'Neue Burg', link: ''});
+            that.burg = that.burgen[that.burgen.length-1];
+            sortiere();
+        }
+
+        $scope.$watch(function() { return that.spielerName; }, function(newValue) {
+            localStorage['e4z9.lak.spielerName'] = newValue;
+        });
+        $scope.$watch(function() { return that.burgen; }, function(newValue) {
+            sortiere();
+            localStorage['e4z9.lak.burgen'] = JSON.stringify(newValue);
+        }, true /*object equality*/);
     });
