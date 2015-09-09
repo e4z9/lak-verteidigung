98017ea386f47218cd138035cd1e22420a750a6a
Eike <e4z9mail@gmail.com>
2015-09-09 09:10:03 +0200

    Meldungen: Angriffsinfo pro Burg speichern, versionierte Settings

--- meldung/app.js
+++ meldung/app.js
@@ -1,36 +1,49 @@
 "use strict";
 angular.module('lakmeldung', [])
     .controller('appController', function($scope) {
+        var that = this;
+        this.burgen = [];
+
+        var createBurg = function() {
+            return { name: 'Neue Burg', link: '',
+                     datum: new Date(), zeit: new Date(),
+                     brueckenLink: '', angreifer: '' };
+        };
+
+        var ladeBurgen = function() {
+            try {
+                var burgSettings = JSON.parse(localStorage['e4z9.lak.burgen']);
+                if (burgSettings !== undefined && burgSettings['burgen'] !== undefined)
+                    that.burgen = burgSettings.burgen;
+            } catch(e) {
+            }
+            if (that.burgen.length <= 0)
+                that.burgen = [createBurg()];
+        };
+        var speicherBurgen = function() {
+            var burgSettings = { version: 1,
+                                 burgen: that.burgen };
+            localStorage['e4z9.lak.burgen'] = JSON.stringify(burgSettings);
+        };
+
         this.spielerName = localStorage['e4z9.lak.spielerName'];
         if (this.spielerName === undefined)
             this.spielerName = '';
 
-        try {
-			this.burgen = JSON.parse(localStorage['e4z9.lak.burgen']);
-	    } catch(e) {
-			this.burgen = [{name: 'Neue Burg', link: ''}];
-	    }
+        ladeBurgen();
         this.burg = this.burgen[0];
-        this.datum = new Date();
-        this.zeit = new Date();
-        this.brueckenLink = '';
-        this.angreifer = '';
-
-        var that = this;
 
         var sortiere = function() {
             that.burgen.sort(function(a, b) {
                 return a.name.localeCompare(b.name);
             });
-        }
-
-        sortiere();
+        };
 
         this.neueBurg = function() {
-            that.burgen.push({name: 'Neue Burg', link: ''});
+            that.burgen.push(createBurg());
             that.burg = that.burgen[that.burgen.length-1];
             sortiere();
-        }
+        };
 
         this.burgEntfernen = function() {
             if (that.burgen.length <= 1)
@@ -40,13 +53,13 @@ angular.module('lakmeldung', [])
                 that.burgen.splice(index, 1);
                 that.burg = that.burgen[Math.min(that.burgen.length-1, index)];
             }
-        }
+        };
 
         $scope.$watch(function() { return that.spielerName; }, function(newValue) {
             localStorage['e4z9.lak.spielerName'] = newValue;
         });
         $scope.$watch(function() { return that.burgen; }, function(newValue) {
             sortiere();
-            localStorage['e4z9.lak.burgen'] = JSON.stringify(newValue);
+            speicherBurgen();
         }, true /*object equality*/);
     });
