a2704d2a45fd68081f2a3c94c5b40a0496c32225
Eike <e4z9mail@gmail.com>
2015-10-29 07:47:17 +0100

    generate.sh fand die sourcen nicht aus einem anderen Verzeichnis

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-multimarkdown "`dirname '$0'`/HandbuchDerVerteidigung.md" | sed 's@<head>@<head>\
+multimarkdown "`dirname "$0"`/HandbuchDerVerteidigung.md" | sed 's@<head>@<head>\
 	<meta name="viewport" content="width = device-width"/><!-- scaling on iphone -->\
 	<link rel="stylesheet" href="stylesheet.css" type="text/css">@'
